5b50f53ab0a49ced619225361d4cf61daa491151
Akira <akame913@gmail.com>
2014-07-04 21:30:26 +0900

    Finish delete product function

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,25 +17,24 @@ User.create(name: 'Akira Kameyama', email: 'akame913@gmail.com',
 # set Product
 #Product.delete.all
 #
-Product.create(title: '業務システム：設計審査', description: 
-%{設計審査として必要な項目をテンプレート化して、設計審査を補助する。<br />
-項目は<strong>ISO9001の検証と妥当性確認</strong>に準拠
-<ul>
-<li>開発言語</li>
-なでしこ
-<li>データーベース</li>
-Sqlite3
-</ul>}, image_url: 'wfs-0.png')
+#Product.create(title: '業務システム：設計審査', description: 
+#%{設計審査として必要な項目をテンプレート化して、設計審査を補助する。<br />
+#項目は<strong>ISO9001の検証と妥当性確認</strong>に準拠
+#<ul>
+#<li>開発言語</li>
+#なでしこ
+#<li>データーベース</li>
+#Sqlite3
+#</ul>}, image_url: 'wfs-0.png')
 #
-Product.create(title: '業務システム：不適合是正', description:
-%{品質ISOの不具合是正処理として必要な項目をテンプレート化して不具合是正処理を補助します。<br />
-ISO9000:2000(JISQ9000:2006)では、<strong>是正処置</strong>について以下のように定義しています。<br />
-検出された不適合またはその他の検出された望ましくない状況の原因を除去するための処置。」
-<ul>
-<li>開発言語</li>
-なでしこ
-<li>データーベース</li>
-Sqlite3
-</ul>}, image_url: 'wfs-1.png')
-#
-     
+#Product.create(title: '業務システム：不適合是正', description:
+#%{品質ISOの不具合是正処理として必要な項目をテンプレート化して不具合是正処理を補助します。<br />
+#ISO9000:2000(JISQ9000:2006)では、<strong>是正処置</strong>について以下のように定義しています。#<br />
+#検出された不適合またはその他の検出された望ましくない状況の原因を除去するための処置。」
+#<ul>
+#<li>開発言語</li>
+#なでしこ
+#<li>データーベース</li>
+#Sqlite3
+#</ul>}, image_url: 'wfs-1.png')
+#     
--- spec/requests/picture_pages_spec.rb
+++ spec/requests/picture_pages_spec.rb
@@ -127,10 +127,11 @@ describe "Picture Pages" do
       let(:new_comment)  { "New comment" }
       before do
         fill_in "Comment",  with: new_comment
+        attach_file "Uploaded picture", "#{Rails.root}/spec/test-image.gif"
         click_button "Save changes"
       end
 
-      it { should have_title(picture.name) }
+      it { should have_title('test-image.gif') }
       it { should have_selector('div.alert.alert-success') }
       specify { expect(picture.reload.comment).to  eq new_comment }
     end
